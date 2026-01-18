2455d1beb9c9da7a6835b24376bcc320730435e7
Pedro Lunet <pedrolunet@gmail.com>
2026-01-18 16:51:28 +0000

    defined and updated colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="h-screen w-screen bg-zinc-950 text-white p-8 box-border overflow-hidden">
+<div class="box-border h-screen w-screen overflow-hidden bg-iridium p-8 text-rose">
 	<div class="h-full w-full">
 		{@render children()}
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,36 @@
 <script lang="ts">
-    const currentYear = new Date().getFullYear();
+	const currentYear = new Date().getFullYear();
 
-    function getDaysInYear(year: number) {
-        const date = new Date(year, 0, 1);
-        const days = [];
+	function getDaysInYear(year: number) {
+		const date = new Date(year, 0, 1);
+		const days = [];
 
-        while (date.getFullYear() === year) {
-            days.push(new Date(date));
-            date.setDate(date.getDate() + 1);
-        }
-        return days;
-    }
+		while (date.getFullYear() === year) {
+			days.push(new Date(date));
+			date.setDate(date.getDate() + 1);
+		}
+		return days;
+	}
 
-    const allDays = getDaysInYear(currentYear);
+	const allDays = getDaysInYear(currentYear);
 
-    const formatDateId = (date: Date) => {
-        return date.toISOString().split('T')[0];
-    };
+	const formatDateId = (date: Date) => {
+		return date.toISOString().split('T')[0];
+	};
 </script>
 
 <div class="h-full w-full">
-    
-    <div class="grid h-full w-full grid-cols-[repeat(19,1fr)] place-content-evenly place-items-center">
-        {#each allDays as day}
-            <button
-                class="h-1 w-1 rounded-full bg-white transition-all duration-300 hover:scale-[3]"
-                aria-label={day.toDateString()}
-                title={day.toDateString()}
-                on:click={() => console.log('Clicked:', formatDateId(day))}
-            >
-            </button>
-        {/each}
-    </div>
-
+	<div
+		class="grid h-full w-full grid-cols-[repeat(19,1fr)] place-content-evenly place-items-center"
+	>
+		{#each allDays as day}
+			<button
+				class="h-1 w-1 rounded-full bg-rose transition-all duration-300 hover:scale-[3]"
+				aria-label={day.toDateString()}
+				title={day.toDateString()}
+				on:click={() => console.log('Clicked:', formatDateId(day))}
+			>
+			</button>
+		{/each}
+	</div>
 </div>
