b654c77367de682dc06089ebc2d5ea731fc82342
Pedro Lunet <pedrolunet@gmail.com>
2026-01-22 11:47:14 +0000

    updated layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,7 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
-<div
-	class="box-border flex h-screen w-screen flex-col overflow-hidden bg-iridium p-6 text-rose md:p-12"
->
+<div class="box-border flex h-screen w-screen flex-col overflow-hidden bg-iridium">
 	<div class="relative h-full w-full">
 		{@render children()}
 	</div>
